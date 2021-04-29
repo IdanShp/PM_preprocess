5702502697693f0bf3ee8b67d4c90b440bbfd04c
Snir Yehuda <SnirYehud@Gmail.com>
2021-04-29 11:46:30 +0300

    files infrastructure, resources and plans

---
+++ main.py
@@ -0,0 +1,31 @@
+
+#TODO give an option to create everything again
+
+# 0.load configuration
+# source steps file. (import steps as ...)
+
+# 1.prepare files
+    # unzip (if necessary)
+    # (convert to csv)?
+
+# 2.prepare data - load with pandas.
+    # filter wanted matches
+    # assign team names
+    # assign player names
+    # convert position to zone
+    # convert time
+    # drop unused columns
+    # assign case id (use deticated function)
+    # filter only wanted cases
+    # create xes file?
+ 
+''' 
+in step 2, at any point, we can save the result to a file
+and then begin from there instead
+of creating again from zero
+''' 
+# 3.analyze data with pm4py
+
+
+    
+    
---
+++ steps.py
@@ -0,0 +1,5 @@
+'''
+here we will write the declerations of the functions.
+than we will source the code to main and use it there
+'''
+
