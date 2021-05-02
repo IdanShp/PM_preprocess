f54d33b1aef082be8510e3f88c6570cc622ce06f
Idan Shpirer <Shpirer.idan@Gmail.com>
2021-05-02 20:38:34 +0300

    nothing important

--- main.py
+++ main.py
@@ -5,48 +5,47 @@ import pandas as pd
 # time for monitoring
 import time
 
-
-#TODO give an option to create everything again
+# TODO give an option to create everything again
 
 # 0.load configuration
-import configurations as conf  
+import configurations as conf
 # source steps file. (import steps as ...)
 import steps as step
 
 start_time = time.process_time()
 
 # 1.prepare files
-    # unzip (if necessary)
-already_unzipped=1
-for f in [conf.events_file,conf.players_file,conf.teams_file,conf.matches_file]:
+# unzip (if necessary)
+already_unzipped = 1
+for f in [conf.events_file, conf.players_file, conf.teams_file, conf.matches_file]:
     if not os.path.exists(f):
-        already_unzipped=0
+        already_unzipped = 0
         break
-    
+
 if not already_unzipped:
     print("extracting files from zip")
     import zipfile
+
     with zipfile.ZipFile(conf.resources_arch, 'r') as zip_ref:
         zip_ref.extractall(conf.resources_dir)
 
-
-
 # 2.prepare data - load with pandas.
 df = pd.read_json(conf.events_file)
-    # filter wanted matches
+# filter wanted matches
 df = df.loc[df[conf.match_col].isin(conf.matches_include)]
-    # assign team names
-df = step.add_team_name(df, conf.teamid_col, conf.team_name_col, conf.teams_file , conf.namefile_id_col, conf.namefile_name_col)
+# assign team names
+df = step.add_team_name(df, conf.teamid_col, conf.team_name_col, conf.teams_file, conf.namefile_id_col,
+                        conf.namefile_name_col)
 
-    # assign player names
+# assign player names
 
-    # convert position to zone
-df = step.add_zone_col(df,conf.position_col,conf.zone_col, conf.zones ,conf.split_x_to, conf.split_y_to)
-    # convert time
-    # drop unused columns
-    # assign case id (use deticated function)
-    # filter only wanted cases
-    # create xes file?
+# convert position to zone
+df = step.add_zone_col(df, conf.position_col, conf.zone_col, conf.zones, conf.split_x_to, conf.split_y_to)
+# convert time
+# drop unused columns
+# assign case id (use deticated function)
+# filter only wanted cases
+# create xes file?
 
 print(df)
 
@@ -54,11 +53,32 @@ print(df)
 in step 2, at any point, we can save the result to a file
 and then begin from there instead
 of creating again from zero
-''' 
+'''
 # 3.analyze data with pm4py
 
 print(time.process_time() - start_time)
 
+# steps:
+# filter season with wanted matches (by match id in config file)
+# open filtered json as dataframe
 
-    
-    
+# convert tags from dict to list
+# read player id, add player name column
+# read team id, add team name column
+# match id - do nothing
+# read position (coordinates) and convert to frame
+# consider - event second, match half and last game finish time. calculate event absolute time and relative time
+# save file
+
+# open file
+# set case ID by - team name, tags, event name.
+# save file
+
+# open file
+# filter High value cases
+# filter short moves (optional)
+# save file
+
+# open file
+# add trace start and end states (optional)
+# save file
--- steps.py
+++ steps.py
@@ -11,9 +11,9 @@ import ast
 
 ##### add zone #####
 
-#gets point and return zone.
-def get_zone_by_point(point,zones,split_x_to,split_y_to):
-    x,y=point
+# gets point and return zone.
+def get_zone_by_point(point, zones, split_x_to, split_y_to):
+    x, y = point
     seg_x = 0
     seg_y = 0
     for i, segment_len in enumerate(split_x_to):
@@ -27,73 +27,96 @@ def get_zone_by_point(point,zones,split_x_to,split_y_to):
             break
     zone = zones[j][i]
     return zone
-    
-def add_zone_col(df,pos_col_name,new_pos_col_name,zones,split_x_to,split_y_to):
 
-    #check col_name label exists
+
+def add_zone_col(df, pos_col_name, new_pos_col_name, zones, split_x_to, split_y_to):
+    # check col_name label exists
     columnsNamesArr = df.columns.values
     listOfColumnNames = list(columnsNamesArr)
     if not pos_col_name in listOfColumnNames:
         print(pos_col_name + "not found in columns.")
         print("use one of those:\n" + listOfColumnNames)
-        return()
-        
-    if  new_pos_col_name in listOfColumnNames:
+        return ()
+
+    if new_pos_col_name in listOfColumnNames:
         print(pos_col_name + "already exists in columns.")
         print("dont one of those:\n" + listOfColumnNames)
-        return()
-    
-    
+        return ()
+
     for i, row in df.iterrows():
-        pos_list = row[pos_col_name] 
+        pos_list = row[pos_col_name]
         # pos_list=ast.literal_eval(positions_str) needed if its a string.
-        
-        #pick the first coordinates
+
+        # pick the first coordinates
         x = pos_list[0]['x']
         y = pos_list[0]['y']
-        zone = get_zone_by_point([x,y],zones ,split_x_to,split_y_to)        
-        df.at[i,new_pos_col_name] = zone
+        zone = get_zone_by_point([x, y], zones, split_x_to, split_y_to)
+        df.at[i, new_pos_col_name] = zone
 
-    return(df)  
+    return (df)
 
 
 ##### Team Name #####
 
 def add_team_name(df, id_col, new_col, name_file_path, nf_id_column, nf_name_col):
-    #   add_name_by_id.main("step3.csv", "teamId", "team_name", "./raw_data/events_Spain.json", "wyId", "officialName", "stage4.1.csv")
+    # add_name_by_id.main("step3.csv", "teamId", "team_name", "./raw_data/events_Spain.json", "wyId", "officialName", "stage4.1.csv")
 
-        
     if not os.path.exists(name_file_path):
-        print("file "+name_file_path+ " dont exist")
-        return()
-        
+        print("file " + name_file_path + " dont exist")
+        return ()
+
     names_df = pd.read_json(name_file_path)
-    
-    #check col_name label exists
+
+    # check col_name label exists
     columnsNamesArr = df.columns.values
     listOfColumnNames = list(columnsNamesArr)
     if not id_col in listOfColumnNames:
         print(id_col + "not found in columns.")
         print("use one of those:\n" + listOfColumnNames)
-        return()
-        
-    if  new_col in listOfColumnNames:
+        return ()
+
+    if new_col in listOfColumnNames:
         print(new_col + "already exists in columns.")
         print("dont one of those:\n" + listOfColumnNames)
-        return()
+        return ()
 
-    #make dic from df
+    # make dic from df
     names_id = names_df.set_index(nf_id_column).to_dict('index')
-        
-    df_size=df.size
+
+    df_size = df.size
     for i, row in df.iterrows():
-        id=row[id_col]
+        id = row[id_col]
         name = names_id[id][nf_name_col]
-        if len(name)==0:
-            name="ID" + str(id)
-        if i%10000 == 0:
-            print(str(i)+" out of "+str(df_size))
+        if len(name) == 0:
+            name = "ID" + str(id)
+        if i % 10000 == 0:
+            print(str(i) + " out of " + str(df_size))
 
-        df.at[i,new_col] = name
+        df.at[i, new_col] = name
 
-    return(df)  
+    return (df)
+
+# steps:
+# filter season with wanted matches (by match id in config file)
+# open filtered json as dataframe
+
+# convert tags from dict to list
+# read player id, add player name column
+# read team id, add team name column
+# match id - do nothing
+# read position (coordinates) and convert to frame
+# consider - event second, match half and last game finish time. calculate event absolute time and relative time
+# save file
+
+# open file
+# set case ID by - team name, tags, event name.
+# save file
+
+# open file
+# filter High value cases
+# filter short moves (optional)
+# save file
+
+# open file
+# add trace start and end states (optional)
+# save file
